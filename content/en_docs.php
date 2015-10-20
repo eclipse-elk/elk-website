915bc5a2afb8f0d66ab52d08a1ee8f2df8bb722b
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2015-10-20 16:47:03 +0200

    Expanded documentation.

--- content/en_docs.php
+++ content/en_docs.php
@@ -13,9 +13,29 @@
 
 <!-- Main content area -->
 <div id="midcolumn">
-  <h1>Getting Started</h1>
+  <h1>Documentation</h1>
   <p>
-    Stay tuned while we're working on this part of our home page!
+    We're still getting ELK ready for the public.
+    While we're doing that,
+    feel free to browse the documentation we produced
+    while we were still part of the <a href="http://rtsys.informatik.uni-kiel.de/kieler">KIELER project</a>:
   </p>
+  <ul>
+    <li>
+      Back then, the <em>Eclipse Layout Kernel (ELK)</em>
+      was called the <em>KIELER Infrastructure for Meta Layout (KIML)</em>.
+      Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/jgEF">on this page</a>.
+    </li>
+    <li>
+      The layout algorithms were part of the
+      <em>KIELER Layouters (KLay)</em> project.
+      Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/kAEF">on this page</a>.
+    </li>
+    <li>
+      For those of you who want to migrate from KIML to ELK,
+      we have <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/CQHS">a summary of changes</a>
+      that you will need to adapt to.
+    </li>
+  </ul>
 </div>
 <!-- ./end  #midcolumn -->

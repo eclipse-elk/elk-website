4d8e522390823318f47f4f499adc8d8626dfe5c0
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2015-10-29 14:29:35 +0100

    Added target audience info to the homepage.

--- content/en_index.php
+++ content/en_index.php
@@ -109,10 +109,7 @@
     there's a very high probability
     that simply installing the Eclipse Layout Kernel
     will not add automatic layout to the tool
-    (at least not in a way that will be remotely helpful to you);
-    adding automatic layout to a tool
-    usually requires playing around with layout options for a while
-    until everything works well.
+    (at least not in a way that will be remotely helpful to you).
     Instead,
     ask the developers of your tool
     to look into integrating their tool with the Eclipse Layout Kernel.

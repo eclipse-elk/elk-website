e0f3e0630cb74e651c02c8cf4c18e35c9ea785e9
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2016-04-18 16:44:41 +0200

    Updated documentation page.
    Signed-off-by: Christoph Daniel Schulze <cds@informatik.uni-kiel.de>

--- content/en_docs.php
+++ content/en_docs.php
@@ -14,27 +14,31 @@
 <!-- Main content area -->
 <div id="midcolumn">
   <h1>Documentation</h1>
-  <p>
-    We're still getting ELK ready for the public.
-    While we're doing that,
-    feel free to browse the documentation we produced
-    while we were still part of the <a href="http://rtsys.informatik.uni-kiel.de/kieler">KIELER project</a>:
-  </p>
   <ul>
     <li>
-      Back then, the <em>Eclipse Layout Kernel (ELK)</em>
-      was called the <em>KIELER Infrastructure for Meta Layout (KIML)</em>.
-      Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/jgEF">on this page</a>.
+      Our main source of documentation
+      is <a href="https://github.com/eclipse/elk/wiki">our GitHub Wiki</a>.
     </li>
     <li>
-      The layout algorithms were part of the
-      <em>KIELER Layouters (KLay)</em> project.
-      Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/kAEF">on this page</a>.
-    </li>
-    <li>
-      For those of you who want to migrate from KIML to ELK,
-      we have <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/CQHS">a summary of changes</a>
-      that you will need to adapt to.
+      Legacy documentation from when we were still part
+      of the <a href="http://rtsys.informatik.uni-kiel.de/kieler">KIELER project</a>:
+      <ul>
+        <li>
+          Back then, the <em>Eclipse Layout Kernel (ELK)</em>
+          was called the <em>KIELER Infrastructure for Meta Layout (KIML)</em>.
+          Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/jgEF">on this page</a>.
+        </li>
+        <li>
+          The layout algorithms were part of the
+          <em>KIELER Layouters (KLay)</em> project.
+          Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/kAEF">on this page</a>.
+        </li>
+        <li>
+          For those of you who want to migrate from KIML to ELK,
+          we have <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/CQHS">a summary of changes</a>
+          that you will need to adapt to.
+        </li>
+      </ul>
     </li>
   </ul>
 </div>

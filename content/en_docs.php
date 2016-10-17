ce8a4dca5f3a071a2eff3b79b79c8d9e4cb12509
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2016-10-17 13:36:45 +0200

    Made the documentation page a bit more readable.
    Signed-off-by: Christoph Daniel Schulze <cds@informatik.uni-kiel.de>

--- content/en_docs.php
+++ content/en_docs.php
@@ -14,32 +14,36 @@
 <!-- Main content area -->
 <div id="midcolumn">
   <h1>Documentation</h1>
+
+  <h2><a href="https://github.com/eclipse/elk/wiki">GitHub Wiki</a></h2>
+  <p>
+    Start here.
+    This is our main source of documentation.
+  </p>
+
+  <h2>Legacy Documentation</h2>
+  <p>
+    Documentation from our pre-Eclipse time,
+    in case our Wiki is not sufficient.
+  </p>
   <ul>
-    <li>
-      Our main source of documentation
-      is <a href="https://github.com/eclipse/elk/wiki">our GitHub Wiki</a>.
-    </li>
-    <li>
-      Legacy documentation from when we were still part
-      of the <a href="http://rtsys.informatik.uni-kiel.de/kieler">KIELER project</a>:
-      <ul>
-        <li>
-          Back then, the <em>Eclipse Layout Kernel (ELK)</em>
-          was called the <em>KIELER Infrastructure for Meta Layout (KIML)</em>.
-          Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/jgEF">on this page</a>.
-        </li>
-        <li>
-          The layout algorithms were part of the
-          <em>KIELER Layouters (KLay)</em> project.
-          Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/kAEF">on this page</a>.
-        </li>
-        <li>
-          For those of you who want to migrate from KIML to ELK,
-          we have <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/CQHS">a summary of changes</a>
-          that you will need to adapt to.
-        </li>
-      </ul>
-    </li>
+    <ul>
+      <li>
+        Back then, the <em>Eclipse Layout Kernel (ELK)</em>
+        was called the <em>KIELER Infrastructure for Meta Layout (KIML)</em>.
+        Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/jgEF">on this page</a>.
+      </li>
+      <li>
+        The layout algorithms were part of the
+        <em>KIELER Layouters (KLay)</em> project.
+        Documentation can be found <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/kAEF">on this page</a>.
+      </li>
+      <li>
+        For those of you who want to migrate from KIML to ELK,
+        we have <a href="https://rtsys.informatik.uni-kiel.de/confluence/x/CQHS">a summary of changes</a>
+        that you will need to adapt to.
+      </li>
+    </ul>
   </ul>
 </div>
 <!-- ./end  #midcolumn -->

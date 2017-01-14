a9f3d1f7a07a1b65fcec30e7e745dc7608913fa5
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2017-01-14 13:07:36 +0100

    Added link to list of resolved issues for release 0.1.0 to downloads page.

--- content/en_downloads.php
+++ content/en_downloads.php
@@ -33,6 +33,7 @@
 
   <h2>Release 0.1.0</h2>
   <ul>
+    <li><a href="https://github.com/eclipse/elk/issues?utf8=%E2%9C%93&q=is%3Aclosed%20milestone%3A%22Release%200.1.0%22">Release log</a></li>
     <li><a href="http://download.eclipse.org/elk/updates/releases/0.1.0/">Update site</a></li>
     <li><a href="http://download.eclipse.org/elk/updates/releases/0.1.0/elk-0.1.0.zip">Zipped update site</a> (for offline use)</li>
     <li><a href="http://download.eclipse.org/elk/maven/releases/0.1.0">Maven Repository</a> (for meta data language compiler)</li>
